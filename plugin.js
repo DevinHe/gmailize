755f4ff0cb53b39901ec17bbf50c1a5a2ff47216
Derek McKinnon <mckinnon.derek@gmail.com>
2014-06-24 13:29:18 -0400

    Added plugin functionality and a test page.

--- plugin.js
+++ plugin.js
@@ -1,2 +1,28 @@
 Selectize.define('gmailize', function(options) {
+	this.require('remove_button');
+
+	if (this.settings.mode === 'single') {
+		return;
+	}
+
+	options = $.extend({		
+		invalidEmailClassName: 'gmailize-invalid',
+		validate: function(email) {
+			return true;
+		}
+	}, options);	
+
+	var self = this;
+
+	this.addItem = (function() {					
+		var original = self.addItem;
+		return function() {			
+			original.apply(this, arguments);
+			var value = arguments[0];
+			if (!options.validate(value)) {
+				var $item = self.getItem(value);
+				$item.addClass(options.invalidEmailClassName);
+			}		
+		};
+	})();
 });
